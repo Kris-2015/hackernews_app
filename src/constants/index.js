4685ed03dea43cc4d67f872cc84eb959f8fa7788
kris batar <kris.batar@b2software.com>
2018-08-06 18:28:51 +0530

    pwa Updated api call url

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,7 @@
 const globalVariable = {
     DEFAULT_QUERY:  'redux',
     DEFAULT_HPP: 20,
-    PATH_BASE: 'http://hn.algolia.com/api/v1',
+    PATH_BASE: 'https://hn.algolia.com/api/v1',
     PATH_SEARCH: '/search',
     PARAM_SEARCH: 'query=',
     PARAM_PAGE: 'page=',
