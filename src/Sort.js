564a153629ea202579bae2264bfd53c0f614565e
kris batar <kris.batar@b2software.com>
2018-08-13 16:54:30 +0530

    development Add Sort component and update the Table component regarding changes

---
+++ src/Sort.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import classNames from "classnames";
+import Button from './Button';
+
+
+/**
+ * @purpose Stateless component which perform sorting
+ * of results.
+ * @param sortKey
+ * @param activeSortKey
+ * @param onSort
+ * @param children
+ * @return Button Component
+ */
+const Sort = ({
+                  sortKey,
+                  activeSortKey,
+                  onSort,
+                  children
+              }) => {
+
+    const sortClass = classNames(
+        'button-inline',
+        { 'button-active' : sortKey === activeSortKey }
+    );
+
+    return (
+        <Button
+            onClick={() => onSort(sortKey)}
+            className={ sortClass }
+        >
+            {children}
+        </Button>
+    );
+};
+
+export default Sort;
--- src/Table.js
+++ src/Table.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import Button from './Button';
 import { sortBy } from 'lodash'
-import classNames from 'classnames';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import constants from './constants/constants';
+import Sort from './Sort';
 
 /**
  * @class Table
@@ -91,28 +91,6 @@ class Table extends Component
             </div>
         );
     };
-}
-
-const Sort = ({
-                sortKey,
-                activeSortKey,
-                onSort,
-                children
-    }) => {
-
-    const sortClass = classNames(
-        'button-inline',
-        { 'button-active' : sortKey === activeSortKey }
-    );
-
-    return (
-        <Button
-            onClick={() => onSort(sortKey)}
-            className={ sortClass }
-        >
-            {children}
-        </Button>
-    );
 }
 
 export default Table;
