7cecb2c3d8aab4662dcc810fa144082ba11a605d
kris batar <kris.batar@b2software.com>
2018-08-16 16:21:54 +0530

    development Upgraded bootstrap version to version 4

--- src/index.js
+++ src/index.js
@@ -2,10 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import 'jquery/src/jquery';
-import 'jquery/dist/jquery.slim.min';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faStroopwafel, faTrash, faSearch, faSpinner } from '@fortawesome/free-solid-svg-icons'
 import registerServiceWorker from './registerServiceWorker';
