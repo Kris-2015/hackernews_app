4468b4daf552eb9671951abbaae12fbb29f44914
kris batar <kris.batar@b2software.com>
2018-08-30 11:13:18 +0530

    Remove extra white space from config file

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 const config = {
-    DEFAULT_QUERY:  process.env.REACT_APP_DEFAULT_QUERY,
+    DEFAULT_QUERY: process.env.REACT_APP_DEFAULT_QUERY,
     DEFAULT_HPP: process.env.REACT_APP_DEFAULT_HPP,
     PATH_BASE: process.env.REACT_APP_PATH_BASE,
     PATH_SEARCH: process.env.REACT_APP_PATH_SEARCH,
